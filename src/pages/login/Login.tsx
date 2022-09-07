92b255b95ad65f62698a7409a5ad6da12b993e15
sojeongsojeong-park <psj90972@naver.com>
2022-09-07 11:17:22 +0900

    feat:implement login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Login.module.css";
 
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.type === "email") {
+      setEmail(event.target.value);
+    } else if (event.target.type === "password") {
+      setPassword(event.target.value);
+    }
+  };
+  const handleSubmit = (event:React.FormEvent<HTMLFormElement> ) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className={styles.login_form} onSubmit={handleSubmit}>
+      <fieldset>
+        <legend> Login </legend>
+        <label htmlFor='myEmail'> Email : </label>
+        <input
+          type='email'
+          id='myEmail'
+          value={email}
+          onChange={handleData}
+          required
+        />
+        <label htmlFor='myPassword'> Password : </label>
+        <input
+          type='password'
+          id='myPassword'
+          value={password}
+          onChange={handleData}
+          required
+        />
+        <button type='submit' className={styles.button}>
+          Login
+        </button>
+      </fieldset>
+    </form>
+  );
 }
 
 export default Login;
